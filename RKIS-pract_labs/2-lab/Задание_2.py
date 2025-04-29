95476f3d62adc34d35dfcc505e7250df3e51d50f
asick0 <sageev.maksim@mail.ru>
2025-04-29 11:05:37 +0700

    поезда

---
+++ RKIS-pract_labs/2-lab/Задание_2.py
@@ -0,0 +1,25 @@
+class Train:
+    def __init__(self, destination, train_number, departure_time):
+        self.destination = destination
+        self.train_number = train_number
+        self.departure_time = departure_time
+    def show_info(self):
+        print(f"\nИнформация о поезде:")
+        print(f"Номер поезда: {self.train_number}")
+        print(f"Пункт назначения: {self.destination}")
+        print(f"Время отправления: {self.departure_time}")
+if __name__ == "__main__":
+    trains = [
+        Train("Москва", "101", "18:20"),
+        Train("Санкт-Петербург", "102", "20:31")
+    ]
+    while True:
+        print("\nПоиск информации о поезде")
+        train_number = input("Введите номер поезда (или '0' для выхода): ")
+
+        if train_number.lower() == '0':
+            break
+        for train in trains:
+            if train.train_number == train_number:
+                train.show_info()
+                break
